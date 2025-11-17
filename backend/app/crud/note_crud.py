61d5a0ff91789a67f65fd4e6916468a2a24d6bbb
Arslan Rejebov <arslanrejepov174@gmail.com>
2025-11-17 23:20:16 +0800

    updateNote_added

--- backend/app/crud/note_crud.py
+++ backend/app/crud/note_crud.py
@@ -16,5 +16,18 @@ def noteCreate(db : Session, note_data:NoteCreate):
 def getNotes(db:Session):
     return db.query(Note).all()
 
-def getIDNote(db:Session,ID:int):
+def getNote(db:Session,ID:int):
     return db.query(Note).filter(Note.id==ID).first()
+
+def updateNote(db:Session, ID:int, note_data:NoteUpdate):
+    note = getNote(db,ID)
+    if not note:
+        return None
+    if note_data.title is not None:
+        note.title=note_data.title
+    if note_data.content is not None:
+        note.content=note_data.content
+    
+    db.refresh(note)
+    db.commit()
+    return note

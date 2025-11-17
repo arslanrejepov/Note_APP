ddc63e035ffe9eed01faa3e1dffec7b4125f04e9
Arslan Rejebov <arslanrejepov174@gmail.com>
2025-11-17 23:33:49 +0800

    delete_Function

--- backend/app/crud/note_crud.py
+++ backend/app/crud/note_crud.py
@@ -23,11 +23,21 @@ def updateNote(db:Session, ID:int, note_data:NoteUpdate):
     note = getNote(db,ID)
     if not note:
         return None
+    
     if note_data.title is not None:
         note.title=note_data.title
     if note_data.content is not None:
         note.content=note_data.content
-    
-    db.refresh(note)
+
     db.commit()
+    db.refresh(note)
     return note
+
+def deleteNote(db:Session,note_id:int):
+    note=getNote(db,note_id)
+    if not note:
+        return None
+    db.delete(note)
+    db.commit()
+    return True
+
